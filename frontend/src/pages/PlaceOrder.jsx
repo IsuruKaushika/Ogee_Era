d002d1dead8fa8315eee1782ade12cabdac549b7
Isuru Kaushika <kawshikaisuru@gmail.com>
2025-04-24 18:27:11 +0530

    -hash

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -57,7 +57,6 @@ const PlaceOrder = () => {
       address: orderData.address.street,
       city: orderData.address.city,
       country: orderData.address.country,
-      hash: hash,
     };
 
     // Create a form and submit it to PayHere
