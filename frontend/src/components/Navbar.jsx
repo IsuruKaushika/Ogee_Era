4f66d7a9462cff4ba6b4981384c9687c9b653867
Isuru Kaushika <157825229+IsuruKaushika@users.noreply.github.com>
2025-10-21 12:15:31 +0530

    NavbarMod

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -49,11 +49,11 @@ const Navbar = () => {
                 <img onClick={()=>token? null:navigate('/login')} className='w-5 cursor-pointer'src={assets.profile_icon} alt=""/>
                 {/*dropdown menu*/}
                 {token &&
-                  <div className='group-hover:block hidden absolute dropdown-menu right-0 top-0 pt-6'>
+                  <div className='group-hover:block hidden absolute dropdown-menu right-0 top-0 pt-6 z-10'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-                        <p className='cursor-pointer hover:text-black'>My Profile</p>
-                        <p onClick={()=>navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
-                        <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
+                        <p className='cursor-pointer hover:text-black' onClick={(e) => e.stopPropagation()}>My Profile</p>
+                        <p onClick={(e) => {e.stopPropagation(); navigate('/orders')}} className='cursor-pointer hover:text-black'>Orders</p>
+                        <p onClick={(e) => {e.stopPropagation(); logout()}} className='cursor-pointer hover:text-black'>Logout</p>
                     </div>
                 </div> }
             </div>
